64fd078da5d8415b3f86bf52f502de031d1ce97c
Sainath Reddy <gsnr1925@gmail.com>
2026-09-07 19:33:22 +0530

    Update starknet-rust-providers/src/jsonrpc/transports/http.rs
    Co-authored-by: Franciszek Job <54181625+franciszekjob@users.noreply.github.com>

--- starknet-rust-providers/src/jsonrpc/transports/http.rs
+++ starknet-rust-providers/src/jsonrpc/transports/http.rs
@@ -27,7 +27,7 @@ pub enum HttpTransportError {
     /// Unexpected response ID.
     #[error("unexpected response ID: {0}")]
     UnexpectedResponseId(u64),
-    /// Response carried a non-numeric (string or null) id that can't be matched to a request.
+    /// Response carried not a valid numeric id that can't be matched to a request.
     #[error("response has a non-numeric id")]
     NonNumericResponseId,
 }
